8e7efbbfe25780e931e364043ceaca69f1fbfcad
Viveckh <vpandey@ramapo.edu>
2016-12-10 01:56:05 -0500

    Techical manual Ready

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 from Tournament import Tournament
 
+print "***************************************************************\n"
+print "*****************WELCOME TO DUELL*****************************\n"
+print "***************************************************************\n\n"
+
 #Entry Point of the application
 tournament = Tournament()
 tournament.play_tournament()
